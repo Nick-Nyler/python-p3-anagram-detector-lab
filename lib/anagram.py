118189e5911e4569e55ac459ccb2c478a1d79b5e
Nick <nicknyler@gmail.com>
2025-05-13 14:42:38 +0300

    initial commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word) 
+
+    def match(self, possible_anagrams):
+        anagrams = []
+        for candidate in possible_anagrams:
+            candidate_lower = candidate.lower()
+            if candidate_lower != self.word and sorted(candidate_lower) == self.sorted_word:
+                anagrams.append(candidate)
+        return anagrams
